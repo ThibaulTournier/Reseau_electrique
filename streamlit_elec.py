29f1843c3c2bfa1a08a2cfad417289375c61c26b
ThibaulTournier <96449633+ThibaulTournier@users.noreply.github.com>
2021-12-29 22:47:58 +0100

    Update streamlit_elec.py

--- streamlit_elec.py
+++ streamlit_elec.py
@@ -10,6 +10,7 @@ df = df.set_index('Date et Heure')
 st.sidebar.title("Panorama du réseau électrique français")
 st.sidebar.subheader("Menu")
 parties_menu = ["Boites à moustache des filières de production",
+                "Boites à moustache mensuelles des filières de production et de la consommation"
                 "Courbes des filières de production",
                 "Carte des filières de production par région",
                 "Prédiction de la consommation d'une région comme série temporelle",
@@ -28,8 +29,29 @@ if choix_menu==parties_menu[0]:
     fig1 = plt.figure(figsize=(15,8))
     ax1 = fig1.add_subplot(111)
     ax1.boxplot(df.loc[str(annee),'Fioul (MW)' : 'Bioénergies (MW)'].resample("D").mean(), showfliers=False)
-    ax1.set_xticklabels(['Fioul','Charbon','Gaz','Nucléaire','Eolien','Solaire','Hydraulique','Pompage','Bioénergies'])
+    ax1.set_xticklabels(['Fioul','Charbon','Gaz','Nucléaire','Eolien','Solaire','Hydraulique','Bioénergies'])
     ax1.set_ylim(0, 61000)
     ax1.set_ylabel("Production (MW)")
     ax1.set_title("Moyennes quotidiennes de puissance de production de l'année " + str(annee) )
     st.pyplot(fig1);
+
+    
+elif choix_menu==parties_menu[1]:
+    st.title(parties_menu[1])
+    st.info("Le graphique suivant permet de visuliser la saisonnalité de chacune des filière de production et de la consommation.")
+    
+    
+    choix_filiere = ['Consommation','Nucléaire','Eolien','Solaire','Hydraulique','Bioénergies','Fioul','Charbon','Gaz']
+    filiere = st.selectbox("Choisissez une filière :", options = choix_filiere)   
+    
+    fig2 = plt.figure(figsize=[15,8])
+    ax2 = fig2.add_subplot(111)
+    
+    
+    l=list()
+    for i in df.resample("M").mean().index.month.unique():
+        l.append(df.resample("M").mean()[df.resample("M").mean().index.month == i][filiere])
+    ax2.boxplot(l, showfliers=False)
+    plt.sca(ax2)
+    plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12], ['J', 'F', 'M', 'A','M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'])
+    ax2.set_ylabel("Puissance (MW)");
