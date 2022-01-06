dec1aa374daf8ba07c3fc3cfa1f8dafd2f2ea641
ThibaulTournier <96449633+ThibaulTournier@users.noreply.github.com>
2022-01-06 22:05:24 +0100

    Update streamlit_elec.py

--- streamlit_elec.py
+++ streamlit_elec.py
@@ -19,6 +19,8 @@ parties_menu = ["Boites à moustache des filières de production",
 
 choix_menu = st.sidebar.radio('', options=parties_menu)
 
+df.resample("M").mean()[df.resample("M").mean().index.month == 3]['Hydraulique (MW)'][3]
+
 if choix_menu==parties_menu[0]:
     st.title(parties_menu[0])
     st.info("Le graphique suivant permet de comparer, année après année, l'amplitude de puissance moyenne produite quotidiennement par filière de production.")
