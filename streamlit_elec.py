ce6e84661315867296695cd57e81546e6c2102f7
ThibaulTournier <96449633+ThibaulTournier@users.noreply.github.com>
2022-01-06 19:14:59 +0100

    Update streamlit_elec.py

--- streamlit_elec.py
+++ streamlit_elec.py
@@ -53,8 +53,8 @@ elif choix_menu==parties_menu[1]:
 
     l=list()
     for i in df.resample("M").mean().index.month.unique():
-      st.write(i)
-    #    l.append(df.resample("M").mean()[df.resample("M").mean().index.month == i]['Solaire (MW)'])
+      #st.write(i)
+      l.append(df.resample("M").mean()[df.resample("M").mean().index.month == i]['Solaire (MW)'])
     ax2.plot(df['Solaire (MW)'])
     #plt.sca(ax2)
     #plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12], ['J', 'F', 'M', 'A','M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'])
