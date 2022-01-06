2a5859b28591586ddbaae0fb8397ca64a24f1918
ThibaulTournier <96449633+ThibaulTournier@users.noreply.github.com>
2022-01-06 19:11:40 +0100

    Update streamlit_elec.py

--- streamlit_elec.py
+++ streamlit_elec.py
@@ -51,9 +51,10 @@ elif choix_menu==parties_menu[1]:
 
     df = df.loc["2012":"2020",:]
 
-    #for i in df.resample("M").mean().index.month.unique():
-    #    l.append(df.resample("M").mean()[df.resample("M").mean().index.month == i]['Solaire (MW)'])
-    ax2.plot(df['Solaire (MW)'])
+    l=list()
+    for i in df.resample("M").mean().index.month.unique():
+        l.append(df.resample("M").mean()[df.resample("M").mean().index.month == i]['Solaire (MW)'])
+    ax2.boxplot(l, showfliers=False)
     #plt.sca(ax2)
     #plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12], ['J', 'F', 'M', 'A','M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'])
     ax2.set_ylabel("Consommation (MW)")
