da88fa7ec7a14605a791703cd1246d382652c683
ThibaulTournier <96449633+ThibaulTournier@users.noreply.github.com>
2022-01-06 15:30:43 +0100

    Update streamlit_elec.py

--- streamlit_elec.py
+++ streamlit_elec.py
@@ -85,7 +85,7 @@ elif choix_menu==parties_menu[2]:
     ax3 = fig3.add_subplot(111)
 
     
-    ax3.plot(df.loc[start : end,liste_graphes].resample(pas).mean(), label = liste_graphes)
+    ax3.plot(df.loc[start : end,options].resample(pas).mean(), label = liste_graphes)
     ax3.legend()
     ax3.set_xlabel("Temps")
     ax3.set_ylabel("Puissance (MW)")
